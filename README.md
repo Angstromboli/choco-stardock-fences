23bdf890e7a82dbb3d509a0c07ed9a5a9cc4d1b1
Angstrom <31828813+Angstromboli@users.noreply.github.com>
2019-04-15 11:09:00 -0700

    First Commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # choco-stardock-fences
+I call this an unofficial package because I did not create the program it installs - Stardock Fences.
+
+The installer will pop up a couple windows after installing (for activation and initial program options), but these do not appear to interfere with the continued installation of more chocolatey packages.
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.stardock.us/downloads/public/software/fences/Fences3-sd-setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'Stardock Fences 3'
+
+  checksum      = '178BA67902FC6A05A040ED3D2F537A95'
+  checksumType  = 'md5'
+
+  validExitCodes= @(0, 3010, 1641)
+  silentArgs   = '/s /noreboot'
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
